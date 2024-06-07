64223e2434ce903c5bd760b5490809f2ad8705de
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2024-06-07 13:48:15 -0300

    update repository

--- Crud-car/src/main/java/com/bertoti/firstproject/crud/model/DateCar.java
+++
@@ -1,4 +0,0 @@
-package com.bertoti.firstproject.crud.model;
-
-public record DateCar(String name, Integer year, String color) {
-}
--- Crud-car/src/main/java/com/bertoti/firstproject/crud/model/UpdateCar.java
+++
@@ -1,9 +0,0 @@
-package com.bertoti.firstproject.crud.model;
-
-public record UpdateCar(
-        Integer id,
-        String name,
-        Integer year,
-        String color
-) {
-}
